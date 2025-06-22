3975c883001e8340d65f6b21f538790604e631aa
goksche <36938090+goksche@users.noreply.github.com>
2025-06-22 05:43:50 +0200

    Read secret key from environment

--- config.py
+++ config.py
@@ -3,6 +3,7 @@ import os
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 # Flask-Konfiguration
-SECRET_KEY = "super-secret-key"
+# SECRET_KEY kann per Umgebungsvariable gesetzt werden
+SECRET_KEY = os.environ.get("SECRET_KEY", "change-me")
 SQLALCHEMY_DATABASE_URI = f"sqlite:///{os.path.join(BASE_DIR, 'instance/kassensystem.sqlite')}"
 SQLALCHEMY_TRACK_MODIFICATIONS = False
